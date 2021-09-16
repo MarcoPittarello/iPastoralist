ea16a622abcb9f2e7e0e1766e1bc23a8d274f680
MarcoPittarello <marco.pittarello1987@yahoo.it>
2021-09-16 17:43:21 +0200

    -fixes

--- R/phytosociological_cover_transformation.R
+++ R/phytosociological_cover_transformation.R
@@ -12,7 +12,7 @@
 #' * **"VanDerMaarel"**: 
 #' * **"TassTapp"**: modified Braun-Blanquet frequency/abundance scale
 #' @return database cover-abundance values
-#' @references * Braun-Blanquet J (1932) Plant sociology. The study of plant communi- ties. GD Fuller and HS Conard (Eds.). Authorized English transla- tions of 'Pflanzensoziologie'. 1st ed. Printed in the United States of America. New York and London: McGraw-Hill Book Co. Inc.\cr
+#' @references * Braun-Blanquet J (1932) Plant sociology. The study of plant communi- ties. GD Fuller and HS Conard (Eds.). Authorized English translations of 'Pflanzensoziologie'. 1st ed. Printed in the United States of America. New York and London: McGraw-Hill Book Co. Inc.\cr
 #' * Westhoff V, van der Maarel E (1973) The Braun-Blanquet approach. In: Whittaker RH (Ed.) Handbook of vegetation science, part 5, Classi- fication and ordination of communities. W Junk, The Hague: 617– 726. https://doi.org/10.1007/978-94-010-2701-4_20\cr
 #' * Tasser E, Tappeiner U (2005) New model to predict rooting in diverse plant community compositions. Ecological Modelling 185:195-211.\cr
 #' @examples  Structure of the database to use as input:
@@ -29,16 +29,10 @@
 #'             spe5             20
 #'
 #'           Note 1: For species without cover leave the cells empty (NA values)
-#'           Note 2: rare species as "+"
+#'           Note 2: rare species as '+'
 #' @export
 
-
-
-
-#
-
-#funzione
-
+method="VanDerMaaler"
 PhytoCover<-function(database,method){
 veg<-database[,-1]
 rownames(veg)<-database[,1]
@@ -58,7 +52,7 @@ if (method=="BrBl"){
                                                                                           ifelse(x==0,"0","+"))))))))))
 }
 
-else if (method=="VanDerMaler"){
+else if (method=="VanDerMaaler"){
   return(data.frame(apply(veg1,MARGIN = c(1,2),function(x) ifelse(x>=1 && x<=5,3,
                                                        ifelse(x>=6 && x<=25,5,
                                                               ifelse(x>=26 && x<=50,7,
