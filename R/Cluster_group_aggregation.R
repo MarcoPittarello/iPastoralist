579f5750b33d98fd7d297e0f5aa21d0be13ce115
MarcoPittarello <marco.pittarello1987@yahoo.it>
2021-09-17 17:08:57 +0200

    - addition of cluster group aggregation function

---
+++ R/Cluster_group_aggregation.R
@@ -0,0 +1,93 @@
+#' Computation of mean values for cluster groups
+#'
+#' @description \cr
+#'
+#' @param database 
+#' @return database with cover-abundance values
+#' @examples  Structure of the database to use as input:
+#'
+#'       Rows: Vegetation survey
+#'       Columns: species
+#'       Headings and row names must be imported
+#'
+#'         Group	  Spe1	  Spe2	...
+#'           1     12.3	   0.0
+#'           1     0.0     5.0
+#'           1     2.0     0.0
+#'           1     10.4    0.0
+#'           2     0.0     20.7
+#'           2     0.0     2.0
+#'           2     2.3     0.0
+#'           3     0.0     23.3
+#'           3     14.3    0.0
+#'
+#'   Note 1: the column of Group (e.g. Vegetation type, Facies..) 
+#'            has to be in the first position
+#' @export
+
+
+clustGroupAggregation=function(database){
+  
+  options(max.print=5000E5)
+  
+  database<-database
+  #database2<-rbind(database,colnames(database))
+  database_heading<-database2[c(nrow(database2),1:(nrow(database2)-1)),]
+  
+  colnames(database)[1] <- "Group"
+  colnames(database_heading)[1] <- "Group"
+  
+  database_group_numeric<-transform(database,Group=as.numeric(database$Group))
+  id_ok<-data.frame(database[,c("Group")])
+  id_cod<-data.frame(database_group_numeric[,c("Group")])
+  database_id<-cbind(id_ok,id_cod) 
+  unique_values_id<-database_id[!duplicated(database_id), ]
+  colnames(unique_values_id)<-c("Group name","Id")
+  unique_values_id_orderd<- unique_values_id[order(unique_values_id[,2]),]
+  
+  un_2<-unique_values_id_orderd
+  un_3<-unique_values_id_orderd
+  
+  merge<-rbind(unique_values_id_orderd,un_2,un_3)
+  merge_ordered<- merge[order(merge[,2]),]
+  
+  
+  aggregation<-aggregate(database_group_numeric,by=list(database_group_numeric$Group),mean)
+  aggregation_t<-t(aggregation)
+  aggregation_t1<-aggregation_t[-c(1),]
+  
+  df <- data.frame(matrix(ncol = ncol(aggregation_t1)*3,
+                          nrow = nrow(aggregation_t1)))
+  
+  for(k in 1:ncol(aggregation_t1)){   
+    df[,(k*3)-1]=aggregation_t1[,k]
+  } 
+  df2<-df
+  
+  selection_species_names<-as.data.frame(database_heading[1,])
+  species_names<-t(selection_species_names)
+  
+  for(k in 1:ncol(aggregation_t1)){   
+    df2[,((k*3)-2)]=species_names[,1]
+  } 
+  df3<-df2
+  
+  df4<-df3[-c(1),]
+  
+  df_final <- data.frame(matrix(ncol = ncol(df4), 
+                                nrow = nrow(df4)))
+  
+  colnames(df_final)<-merge_ordered[,1]
+  
+  
+  for(k in 1:ncol(aggregation_t1)){
+    d1<-df4[,c(((k*3)-2),(((k*3)-2))+1)]
+    d1_ordered <- d1[order(-d1[,2]),]
+    df_final[,((k*3)-2)]=d1_ordered[,1]
+    df_final[,(((k*3)-2)+1)]=round(d1_ordered[,2],1)
+  }
+  
+  df_final[is.na(df_final)] <- "" 
+  aggregation_cluster.table<-df_final
+  return(aggregation_cluster.table)
+}
---
+++ R/cluster_survey_order.R
@@ -0,0 +1,41 @@
+#' Extraction of the position and/or the group of appartenance of survey in a dendrogram
+#'
+#' @description \cr
+#'
+#' @param database.cluster: database used for generate the cluster. Rows are survey and Columns are species 
+#' @param cluster.hclust: output from 'hclust' function of 'stats' package 
+#' @param cluster.group: Logical. TRUE if you want to specify the number of groups of the dendrogram. 
+#' @param cluster.number: To be specified only if 'cluster.group' is TRUE. Set the number of groups the dendrogram will be divided
+#' @return when 'cluster.group' = TRUE: a dataframe in which for each survey is specified the group id of appartenance and its position in the dendrogram
+#' when 'cluster.group' = FALSE: a dataframe in which for each survey its position in the dendrogram
+#' @export
+
+clustOrder<-function(database.cluster,cluster.hclust,cluster.group,cluster.number){
+  
+  if (cluster.group==TRUE){
+    #database with cluster groups
+    cluster.groups<-data.frame(cutree(cluster.hclust,k=cluster.number))#cut dendrogram with k groups
+    cluster.groups$Survey<-row.names(cluster.groups)#add column with survey name
+    colnames(cluster.groups)[1] <- "cluster"#rename column cluster groups as cluster
+    cluster.groups<-cluster.groups[order(cluster.groups$Survey),]#order db by survey name
+    
+    #database with the survey order in the dendrogram
+    survey.order<-data.frame(cbind(cluster.hclust$labels[c(cluster.hclust$order)],
+                                   c(1:nrow(database.cluster))))#dataframe with survey name and its order in the dendrogram
+    colnames(survey.order)[1] <- "Survey"
+    colnames(survey.order)[2] <- "Survey.order"
+    survey.order<-survey.order[order(survey.order$Survey),]#order db by survey name
+    
+    table<-data.frame(cbind(cluster.groups,survey.order))#merge od database
+    table1<-table[,c(2,1,4)]#selection of columns
+    rownames(table1)<-NULL
+    return(table1)
+    
+  }
+  
+  else if (cluster.group==FALSE){
+    return(data.frame(cbind(cluster$labels[c(cluster$order)],
+                     c(1:nrow(dendro)))))
+  }
+  
+}
