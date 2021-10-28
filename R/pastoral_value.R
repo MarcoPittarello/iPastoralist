f7107e388c141e501331703bdaa644ec023290de
MarcoPittarello <marco.pittarello1987@yahoo.it>
2021-10-28 13:49:46 +0200

    little change in PV computation: species withouth ISQ are classified as 999

--- R/pastoral_value.R
+++ R/pastoral_value.R
@@ -2,7 +2,7 @@
 #'
 #' Computation of PV from a database with SRA, either SRA_fo or SRA_SC.fo.occ (see \link[iPastoralist]{vegetation_abundance})
 #' @param SRA_data database with SRA values, either 'SRA_fo' or 'SRA_SC.fo.occ' (see \link[iPastoralist]{vegetation_abundance}). Database class must be *data.frame*
-#' @param isq_data database with ISQ values. Database class must be *data.frame*
+#' @param isq_data database with ISQ values. Species without an ISQ have to be specified with 999.Database class must be *data.frame*
 #' @return database with PV
 #' @export
 
@@ -10,7 +10,7 @@ pastoral_value=function (SRA_data,isq_data){
   database<-cbind(isq_data,SRA_data)
   
   colnames(database)[1]<-"ISQ"
-  ISQ_selection<-subset(database, ISQ>= 0)# selection of species with ISQ ranging
+  ISQ_selection<-subset(database, ISQ< 999)# selection of species with ISQ ranging
   #between 0 and 1. NA values are excluded
   
   #Extraction of ISQ column from PV_database
@@ -22,5 +22,4 @@ pastoral_value=function (SRA_data,isq_data){
   #computation of PV for each Ril column
   PV<-apply(abundance, 2, function(x) sum(x*ISQ)*0.2)
   PV<<-data.frame(PV)
-  print(PV)
 }
